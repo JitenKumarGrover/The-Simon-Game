1f75baf653f00c433f69d29a5f1eb1955df407df
Jiten Kumar Grover <39979352+JitenKumarGrover@users.noreply.github.com>
2020-06-06 22:25:15 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -6,7 +6,15 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-
+ 
+ $(document).on("tap",function(){
+   if(!started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+  });
+ 
 $(document).keypress(function() {
   if(!started){
     $("#level-title").text("Level " + level);
